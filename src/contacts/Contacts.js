4ffc5ff3e9ec041961916d49739da94e138d1ee6
DinaSem <neispravimayas1@gmail.com>
2022-08-25 01:00:25 +0300

    movie project

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -3,6 +3,8 @@ import s from './Contacts.module.scss';
 import Title from "../common/component/title/Title";
 import st from "../common/styles/Container.module.scss";
 import Fade from 'react-reveal/Fade'
+import email from '.././assets/image/free-animated-icon-message-6416396.gif'
+import telegram from '.././assets/image/telegram.gif'
 
 function Contacts() {
     return (
@@ -11,12 +13,24 @@ function Contacts() {
             <div className={`${st.container} ${s.contactsContainer}`}>
                <Title text={'Contact'} />
 
-                <form className={s.contactsForm}>
-                    <input type="text" className={s.formAria} placeholder={'Your Name'}/>
-                    <input type="text" className={s.formAria} placeholder={'Your E-mail'}/>
-                    <textarea className={s.messageAria} placeholder={'Your Message'}/>
-                    <button type='submit' className={s.submitBtn}>Send Message</button>
-                </form>
+                <h3>Заинтересованы в сотрудничестве?</h3>
+
+                <div className={s.EmailWrapper}>
+                    <img src={email} className={s.contactsEmailGif}/>
+                    <a href="mailto:">d.r.semenova@yandex.ru</a>
+                </div>
+                <div className={s.telegramWrapper}>
+                    <img src={telegram} className={s.telegramGif}/>
+                    <a href='https://t.me/DinaSem'>https://t.me/DinaSem</a>
+                </div>
+
+
+                {/*<form className={s.contactsForm}>*/}
+                {/*    <input type="text" className={s.formAria} placeholder={'Your Name'}/>*/}
+                {/*    <input type="text" className={s.formAria} placeholder={'Your E-mail'}/>*/}
+                {/*    <textarea className={s.messageAria} placeholder={'Your Message'}/>*/}
+                {/*    <button type='submit' className={s.submitBtn}>Send Message</button>*/}
+                {/*</form>*/}
             </div>
             </Fade>
         </div>
--- src/nav/NavFooter.js
+++ src/nav/NavFooter.js
@@ -5,7 +5,7 @@ import s from './NavFooter.module.scss';
 function NavFooter() {
     return (
             <div className={s.nav}>
-                <a href="d.r.semenova@yandex.ru">email</a>
+                <a href="mailto:d.r.semenova@yandex.ru">email</a>
                 <a href="https://github.com/DinaSem">github</a>
                 <a href="https://www.linkedin.com/in/%D0%B4%D0%B8%D0%BD%D0%B0-%D1%81%D0%B5%D0%BC%D0%B5%D0%BD%D0%BE%D0%B2%D0%B0-85bbba87/">linked</a>
                 <a href="https://t.me/DinaSem">telegram</a>
